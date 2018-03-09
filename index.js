885334ae01acbf55d0f089a83876f37456b23380
Haider Malik <haidermalik504@gmail.com>
2018-03-09 19:53:43 +0500

    connecting to mongoose

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 import express from "express";
 import graphqlHTTP from "express-graphql";
 import schema from "./schema";
+import mongoose from 'mongoose';
 
 const app = express();
 
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/graphqdb');
 app.get("/", (req, res) => {
   return res.json({
     msg: "Welcome to GraphQL World!!"
